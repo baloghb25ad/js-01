cd2419fef54e24284abbba2e54912109cf6a4402
baloghb25ad <baloghb.25ad@acsjszki.hu>
2026-03-18 10:35:16 +0100

    2

--- script.js
+++ script.js
@@ -1,6 +1,14 @@
-    //alert("Hello, World!")
-  
+//alert("Hello, World!")
+
 
 function gomb() {
-    document.getElementById("szöveg").innerHTML = "Hello, World!"
+    document.getElementById("szöveg").innerHTML = "Hello, World!";
+}
+
+
+function osszead() {
+    var szam1 = parseFloat(document.getElementById("szam1").value);
+    var szam1 = parseFloat(document.getElementById("szam2").value);
+    var eredmeny = szam1 + szam2
+    document.getElementById("eredmeny").innerHTML = eredmeny;
 }
